e412a3712c39d76ce48551ba044084647fba86f2
gil20 <gilbeeshkosma15@gmail.com>
2019-12-09 23:47:35 +0530

    intro part

---
+++ src/components/project_card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CardStyle from './project_card_css.module.css'
+
+
+
+export default ({children}) => (
+		<div css={{margin:'10px',display:'flex',flexWrap:'wrap'}}>
+		
+		</div>
+	)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,27 @@
 import React from "react"
 import Layout from '../components/layout'
-
+import myicon from '../../static/images/office.svg';
+import ProjectCard from '../components/project_card';
 
 
 export default () => (
 		<Layout>
 		<div>
-			<div css={{display:`flex`,justifyContent:`center`,alignItems:`center`,height:`50vh`}}>
-			     <h2>I'm Gilbish Kosma </h2></br>
-			     <p>Also known as gil20, a self-taught Python Developer,loves drawing and illustrations</p>
+			<div css={{display:`flex`,alignItems:'center'}}>
+				<div css={{display:'flex',width:'100vw',flexWrap:'wrap',boxSizing:'border-box',marginTop:'100px'}}>
+					<div css={{display:'flex',width:'50%',justifyContent:'center',alignItems:'center'}} >
+						<img src={myicon} alt="icon" css={{width:'80%'}} /> 
+					</div>
+					<div css={{display:'flex',flexDirection:'column',width:'50%',justifyContent:'center',alignItems:''}}>
+						<h2>I am Lorem Lipsum</h2>
+						<p>lorem lipsum adasd a sd a d a sdasdas dasdasd asdas dasda </p>
+					</div>
+				</div>
+			</div>
+			<div css={{display:`flex`,alignItems:'center',justifyContent:'center',marginTop:'100px',marginBottom:'100px'}}>
+			    <ProjectCard>
+			    <h1>Project</h1>
+			    </ProjectCard>
 			</div>
 		</div>
 		</Layout>
