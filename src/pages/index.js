ff7df01139e7a997a5c40f43858fea5d975b90f9
gil20 <gilbeeshkosma15@gmail.com>
2019-12-11 20:49:56 +0530

    some change

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,13 @@ import ProjectCard from '../components/project_card';
 
 export default () => (
 		<Layout>
-		<div>
-			<div css={{display:`flex`,alignItems:'center'}}>
-				<div css={{display:'flex',width:'100vw',flexWrap:'wrap',boxSizing:'border-box',marginTop:'100px'}}>
-					<div css={{display:'flex',width:'50%',justifyContent:'center',alignItems:'center'}} >
-						<img src={myicon} alt="icon" css={{width:'80%'}} /> 
+			<div>
+			<div css={{display:`flex`}}>
+				<div css={{display:'flex',flexWrap:'wrap',justifyContent:'center',alignItems:'center',marginTop:'3em'}}>
+					<div css={{display:'flex',justifyContent:'center',alignItems:'center'}} >
+						<img src={myicon} alt="icon" css={{width:'60%'}} /> 
 					</div>
-					<div css={{display:'flex',flexDirection:'column',width:'50%',justifyContent:'center',alignItems:''}}>
+					<div css={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'left'}}>
 						<h2>I am Lorem Lipsum</h2>
 						<p>lorem lipsum adasd a sd a d a sdasdas dasdasd asdas dasda </p>
 					</div>
@@ -23,6 +23,6 @@ export default () => (
 			    <h1>Project</h1>
 			    </ProjectCard>
 			</div>
-		</div>
+			</div>
 		</Layout>
 	)
